a6c2dcb3b7b43704deb82a5214752eb1fc8c0f0a
Abid Hasan kiam <kiamhasan267@gmail.com>
2024-06-09 01:16:40 +0600

    fix

--- src/components/components/Icon.tsx
+++ src/components/components/Icon.tsx
@@ -3,15 +3,27 @@ import Image from 'next/image';
 function Icon() {
   return (
     <>
-      <div className="left-icon mt-4 mt-md-0 ">
-        <a
-          href="https://api.whatsapp.com/send/?phone=%2B19792507486&text&type=phone_number&app_absent=0 "
-          target="_blank"
-        >
-          <i className="fa-brands fa-whatsapp fa-bounce fs-3 px-3 py-3"></i>
-        </a>
+      <div
+        className="left-icon"
+        // style={{
+        //   marginTop: '125px',
+        // }}
+      >
+        <div>
+          <a
+            href="https://api.whatsapp.com/send/?phone=%2B19792507486&text&type=phone_number&app_absent=0 "
+            target="_blank"
+          >
+            <i className="fa-brands fa-whatsapp fa-bounce fs-5  p-3 py-3"></i>
+          </a>
+        </div>
       </div>
-      <div className="left-icon-tow mt-4 mt-md-0">
+      <div
+        className="left-icon-tow "
+        // style={{
+        //   marginTop: '120px',
+        // }}
+      >
         <a
           href="tel:+18884423442"
           target="_blank"
@@ -20,12 +32,12 @@ function Icon() {
           <Image
             src="/phone.gif"
             alt="phone-icon"
-            className="ps-2 "
+            className="ps-1 "
             width={200}
             height={200}
             style={{
               objectFit: 'cover',
-              width: '34px',
+              width: '25px',
             }}
           />
         </a>
