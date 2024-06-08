7d8d3929e1c6a1849bd7cb43abc6092fc077acb3
Abid Hasan kiam <kiamhasan267@gmail.com>
2024-06-09 02:14:11 +0600

    fix

--- src/components/components/Icon.tsx
+++ src/components/components/Icon.tsx
@@ -10,7 +10,7 @@ function Icon() {
       <div
         className="left-icon"
         style={{
-          top: isWide ? '84.5vh' : '78vh',
+          top: isWide ? '84.5vh' : '76vh',
         }}
       >
         <div>
@@ -25,7 +25,7 @@ function Icon() {
       <div
         className="left-icon-tow "
         style={{
-          top: isWide ? '91vh' : '87vh',
+          top: isWide ? '91vh' : '85vh',
         }}
       >
         <a
